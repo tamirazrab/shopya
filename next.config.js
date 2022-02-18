67932a5352a084e9736fa959cb7458b4a62f5cce
Tamir <tamirazrab@gmail.com>
2022-02-19 01:53:43 +0500

    feat: :sparkles: add graphql to fetch products upload on shopify store

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SHOPIFY_STORE_FRONT_ACCESS_TOKEN: process.env.SHOPIFY_STORE_FRONT_ACCESS_TOKEN,
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN
+  },
+  images: {
+    domains: ['cdn.shopify.com']
+  }
 }
