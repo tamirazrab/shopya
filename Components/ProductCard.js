66ca1a73cb0fbdebf161c1b93744031dc9fe14e8
Tamir <tamirazrab@gmail.com>
2022-02-19 02:44:39 +0500

    feat: :sparkles: display all products

---
+++ Components/ProductCard.js
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+import { formatter } from '../utils/helpers'
+
+export const ProductCard = ({ product }) => {
+  console.log("🚀 ~ file: ProductCard.js ~ line 5 ~ ProductCard ~ product", product)
+  const { handle, title } = product.node
+  const { altText, originalSrc } = product.node.images.edges[0].node
+  const price = formatter.format(product.node.priceRange.minVariantPrice.amount)
+
+  return (
+    <Link href={`/product/${handle}`}>
+      <a className="group">
+        <div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
+          <div className="relative group-hover:opacity-75 h-72">
+            <Image src={originalSrc} alt={altText} layout="fill" objectFit="cover" />
+          </div>
+        </div>
+
+        <h3 className="mt-4 text-lg font-medium text-gray-800">{title}</h3>
+        <p className="mt-1 text-sm text-gray-700">{price}</p>
+
+      </a>
+    </Link>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import { getProductsInCollection } from '../lib/shopify'
+import { ProductList } from '../Components/ProductList'
 
-export default function Home({ products}) {
-  return <div className="text-3xl"></div>
+export default function Home({ products }) {
+  return (
+    <div className="text-3xl">
+      <ProductList products={products} />
+    </div>
+  )
 }
 
 export async function getStaticProps() {
